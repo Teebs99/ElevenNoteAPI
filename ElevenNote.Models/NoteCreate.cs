a5f34968e095e60ea435c8511f83ad3fd10a0afb
Stephen Ives <teebs99@gmail.com>
2021-07-15 11:20:46 -0400

    Created Notes and the Note Controller

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.WebAPI.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
